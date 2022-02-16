bd08881a4c298100a6df1910afae7ec902a60d87
Darrienrjohnson <69940765+Darrienrjohnson@users.noreply.github.com>
2022-02-16 18:58:16 -0500

    Create variables
    grabbed key pieces and assigned to variables.

---
+++ app.js
@@ -0,0 +1,14 @@
+const billInput = document.querySelector('#bill');
+const tipButtons = document.querySelectorAll('.tip-button');
+const customTip = document.querySelector('.tip-percentage');
+const numPeopleInput = document.querySelector('#total-people');
+const resetButton = document.querySelector('.reset-button');
+const tipAmountOutput = document.querySelector('output-amount');
+const totalTipAmountOutput = document.querySelector('total-output-amount');
+
+
+//"input" event is an event that happens anytime the content of the input changes. 
+//forEach()
+//parseFloat()
+
+//toFixed() returns a string representation of an object tand has specified number of digits after the decimal place. 
