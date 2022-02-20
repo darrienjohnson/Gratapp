ad9d2e6a04115fb7337f1add374c2fce59693380
Darrienrjohnson <69940765+Darrienrjohnson@users.noreply.github.com>
2022-02-19 23:17:27 -0500

    toggle function
    hard time figuring out toggle function. will have to take it apart and break it down to understand each part

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const billInput = document.querySelector('#bill');
 const tipButtons = document.querySelectorAll('.button');
-const customTip = document.querySelector('.tip-percentage');
+const customTip = document.querySelector('#custom-tip');
 const numPeopleInput = document.querySelector('#total-people');
-const resetButton = document.querySelector('.reset-button');
+const resetButton = document.querySelector('#reset-button');
 const tipAmountOutput = document.querySelector('#amountOutput');
 const totalTipAmountOutput = document.querySelector('#totalOutput');
 
@@ -29,8 +29,58 @@ function calculateTotal() {
   }
 }
 
+tipButtons.forEach(button => {
+  button.addEventListener('click', activeButton);
+});
 
-  
+resetButton.addEventListener('click', () => {
+  reset();
+});
+
+function activeButton(e) {
+  tipButtons.forEach(button => {
+    button.classList.remove('active-button');
+    if (e.target.innerHTML == button.innerHTML) {
+      if (e.target.id !== "reset-button") {
+        button.classList.add('active-button');
+      }
+      percentage = parseFloat(button.innerHTML) / 100
+    }
+  });
+  if (billInputVal !== "") {
+    calculateTotal();
+  }
+  customTip.addEventListener('input', () => {
+    percentage = parseFloat(customTip.value) / 100;
+    calculateTotal();
+  });
+}
+
+function reset() {
+  billInputVal = "";
+  billInput.value = "";
+  numPeopleInput.value = "";
+  numPeopleInputVal = 1;
+  customTip.value = "";
+  percentage = 0.15;
+  tipAmountOutput.textContent = "$0.00";
+  totalTipAmountOutput.textContent = "$0.00";
+  tipButtons.forEach(button => {
+    button.classList.remove('active-button');
+  })
+  tipButtons[2].classList.add('active-button');
+}
+
+// //Add "click" listener to every #button
+// tipButtons.classList.add('active-button')
+// tipButtons.classList.remove('active-button')
+// tipButtons.classList.toggle('active-button')
+
+
+
+
+
+
 //"input" event is an event that happens anytime the content of the input changes. 
 //value property sets or returns the value of an attribute. 
 //forEach() calls a function for each element in array
